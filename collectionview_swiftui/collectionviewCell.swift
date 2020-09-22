89cc7268f64d2ccdfc22f388a731769ebba541aa
dipika <b.dipika106@gmail.com>
2020-09-22 11:58:35 +1000

    Number Grid using Swiftui

---
+++ collectionview_swiftui/collectionviewCell.swift
@@ -0,0 +1,20 @@
+//
+//  collectionviewCell.swift
+//  collectionview_swiftui
+//
+//  Created by Dipika on 22/9/20.
+//
+
+import SwiftUI
+
+struct collectionviewCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct collectionviewCell_Previews: PreviewProvider {
+    static var previews: some View {
+        collectionviewCell()
+    }
+}
