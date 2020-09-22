9ea7a00aa7c398153da812c6ee95211908264e86
dipika <b.dipika106@gmail.com>
2020-09-22 10:45:11 +1000

    Initial Commit

---
+++ collectionview_swiftui/collectionview_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  collectionview_swiftuiApp.swift
+//  collectionview_swiftui
+//
+//  Created by Dipika on 22/9/20.
+//
+
+import SwiftUI
+
+@main
+struct collectionview_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
